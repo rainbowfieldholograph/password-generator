05c7f031faee2f5972b4ccbdbcbfedd4439b5bef
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-06-04 18:33:45 +0900

    code refactor: object used instead of array

--- scripts/generatePassword/generatePassword.ts
+++ scripts/generatePassword/generatePassword.ts
@@ -9,6 +9,12 @@ const arrayFromCharcodes = (low: number, high: number): string[] => {
   return result;
 };
 
+const getRandomInteger = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const SYMBOLS = Array.from('!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~');
 const UPPERCASES = arrayFromCharcodes(65, 90);
 const LOWERCASES = arrayFromCharcodes(97, 122);
@@ -16,25 +22,19 @@ const NUMBERS = arrayFromCharcodes(48, 57);
 
 const MIN_LENGTH = 4;
 
-export const getRandomInteger = (min: number, max: number): number => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 export const generatePassword = (params: GeneratePasswordParams): string => {
   let charsArray: string[] = [];
   const { length, hasNumbers, hasLowerCase, hasUpperCase, hasSymbols } = params;
-  const condsAndChars: [boolean, string[]][] = [
-    [hasLowerCase, LOWERCASES],
-    [hasUpperCase, UPPERCASES],
-    [hasNumbers, NUMBERS],
-    [hasSymbols, SYMBOLS],
+  const condsAndChars: { cond: boolean; chars: string[] }[] = [
+    { cond: hasLowerCase, chars: LOWERCASES },
+    { cond: hasUpperCase, chars: UPPERCASES },
+    { cond: hasNumbers, chars: NUMBERS },
+    { cond: hasSymbols, chars: SYMBOLS },
   ];
 
   if (length < MIN_LENGTH) return '';
 
-  for (const [cond, chars] of condsAndChars) {
+  for (const { cond, chars } of condsAndChars) {
     if (cond) charsArray = [...charsArray, ...chars];
   }
 
@@ -43,9 +43,10 @@ export const generatePassword = (params: GeneratePasswordParams): string => {
     () => charsArray[getRandomInteger(0, charsArray.length)]
   );
 
-  for (const [cond, chars] of condsAndChars) {
+  for (const { cond, chars } of condsAndChars) {
+    if (!cond) continue;
     const passHasChar = password.some((char) => chars.includes(char));
-    if (cond && !passHasChar) return generatePassword(params);
+    if (!passHasChar) return generatePassword(params);
   }
 
   return password.join('');
